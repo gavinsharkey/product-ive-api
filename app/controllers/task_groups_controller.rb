285d0cb14fa29f96f7e2133942d245b3e0e1987b
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-03 12:22:21 -0400

    Add index action to task groups controller

--- app/controllers/task_groups_controller.rb
+++ app/controllers/task_groups_controller.rb
@@ -1,2 +1,8 @@
 class TaskGroupsController < ApplicationController
+  before_action :require_login
+
+  def index
+    @task_groups = session_user.task_groups
+    render json: @task_groups
+  end
 end
