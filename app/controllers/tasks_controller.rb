aff9558a4efdc07b8cfd771ac0e5781a20343ba0
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-06 14:53:14 -0400

    Add index action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_login
+
+  def index
+    user = session_user || User.find_by(id: params[:user_id])
+    if params[:task_group_id]
+      @tasks = user.grouped_tasks.where(taskable_id: params[:task_group_id])
+    else
+      @tasks = user.all_tasks
+    end
+    render json: @tasks, include: [:taskable]
+  end
 end
