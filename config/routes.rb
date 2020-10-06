e0c34ac289be4996a6aa0b62ae531e2ce67eda12
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-06 14:53:35 -0400

    Add index resource actions for tasks, task_groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tasks, only: [:index]
+  resources :task_groups, only: [:index]
   post '/login', to: 'auth#login'
   post '/signup', to: 'users#create'
   get '/logged_in', to: 'auth#is_logged_in?'
