0ccfc430ec2c6ef5cacf2c3cc5b790c8345596a3
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 10:17:31 -0400

    Change task_group_id to taskable_id (matches schema)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,8 @@ class TasksController < ApplicationController
 
   def index
     user = session_user || User.find_by(id: params[:user_id])
-    if params[:task_group_id]
-      @tasks = user.grouped_tasks.where(taskable_id: params[:task_group_id])
+    if params[:taskable_id]
+      @tasks = user.grouped_tasks.where(taskable_id: params[:taskable_id])
     else
       @tasks = user.all_tasks
     end
