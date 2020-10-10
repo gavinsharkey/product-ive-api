74d92d2da0add6213021431c08296d22ea5f0f7b
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 15:44:19 -0400

    Add current_user helper for development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
+  def current_user
+    @user ||= session_user || User.find_by(id: params[:user_id])
+  end
+
   def encode_token(payload)
     JWT.encode(payload, '_somethingsomthing')
   end
