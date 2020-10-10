37ccdc722d8ceca71bb63f1c6816519d9da88013
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 10:18:18 -0400

    Add task_group_name method for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,8 @@ class Task < ApplicationRecord
 
   belongs_to :parent_task, class_name: 'Task', optional: true
   has_many :sub_tasks, class_name: 'Task', foreign_key: 'parent_task_id'
+
+  def task_group_name
+    self.taskable_type == 'TaskGroup' ? self.taskable.name : 'Ungrouped'
+  end
 end
