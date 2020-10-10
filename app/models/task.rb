e718ff775c86eff74d58f4b385a4bae8eb35db45
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 19:52:46 -0400

    Add dependent destroy to subtasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :taskable, polymorphic: true, optional: true
 
   belongs_to :parent_task, class_name: 'Task', optional: true
-  has_many :sub_tasks, class_name: 'Task', foreign_key: 'parent_task_id'
+  has_many :sub_tasks, class_name: 'Task', foreign_key: 'parent_task_id', dependent: :destroy
 
   def task_group_name
     self.taskable_type == 'TaskGroup' ? self.taskable.name : 'Ungrouped'
