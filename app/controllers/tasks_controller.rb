9bf6a2cad2a0d61c42b40b43aef95744cb1ab32d
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-10 15:44:35 -0400

    Add update action Use current_user helper

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,32 @@
 class TasksController < ApplicationController
   before_action :require_login
+  before_action :set_task, only: [:update]
 
   def index
-    user = session_user || User.find_by(id: params[:user_id])
     if params[:taskable_id]
-      @tasks = user.grouped_tasks.where(taskable_id: params[:taskable_id])
+      @tasks = self.current_user.grouped_tasks.where(taskable_id: params[:taskable_id])
     else
-      @tasks = user.all_tasks
+      @tasks = self.current_user.all_tasks
     end
     render json: @tasks, include: [:taskable]
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: @task, include: [:taskable]
+    else
+      render json: { errors: @task.errors.full_messages }
+    end
+  end
+
+  private
+
+  def set_task
+    @task = self.current_user.grouped_tasks.find_by(id: params[:id])
+    render json: { errors: ['Unauthorized access: Not Your Resource'] } if !@task
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :completed)
   end
 end
