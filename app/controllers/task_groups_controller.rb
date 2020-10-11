bd2012a2a1b1fd96bc8f245bd92b02b3bcb25f53
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-11 15:34:36 -0400

    Add create action for task_groups

--- app/controllers/task_groups_controller.rb
+++ app/controllers/task_groups_controller.rb
@@ -5,4 +5,19 @@ class TaskGroupsController < ApplicationController
     @task_groups = current_user.task_groups
     render json: @task_groups
   end
+
+  def create
+    @task_group = current_user.task_groups.build(task_group_params)
+    if @task_group.save
+      render json: @task_group, status: :created
+    else
+      render json: { errors: @task_group.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_group_params
+    params.require(:task_group).permit(:name)
+  end
 end
