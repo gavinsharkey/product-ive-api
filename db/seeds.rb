c1bc9243d054ca6ba32c090080add8169595d7fe
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-11 16:46:14 -0400

    Reseed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.first
+u = User.create(display_name: 'Gavin Sharkey', email: 'sharkeyfamilygm@gmail.com', password: '12345')
 
-5.times do
-  tg = u.task_groups.create(name: Faker::Job.title)
-  u.ungrouped_tasks.create(name: Faker::Food.dish)
-  5.times do
-    tg.tasks.create(name: Faker::Food.dish)
-  end
-end
+tg = u.task_groups.create(name: 'Groceries')
+tg.tasks.create(name: 'Apples')
+tg.tasks.create(name: 'Oatmeal')
+tg.tasks.create(name: 'Bananas')
+tg.tasks.create(name: 'Tortillas')
+tg.tasks.create(name: 'Avocados')
+
+tg2 = u.task_groups.create(name: 'Stuff To Do')
+tg2.tasks.create(name: 'Look for jobs on LinkedIn')
+tg2.tasks.create(name: 'Email some fuckers')
+tg2.tasks.create(name: 'Pack my shit')
+tg2.tasks.create(name: 'work out')
+tg2.tasks.create(name: 'EAT')
