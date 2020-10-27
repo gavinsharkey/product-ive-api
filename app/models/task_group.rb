b28a893bba1a05aec512cbf12914db7ee7cb74e4
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-27 12:56:48 -0400

    Add dependent destroy for tasks

--- app/models/task_group.rb
+++ app/models/task_group.rb
@@ -1,6 +1,6 @@
 class TaskGroup < ApplicationRecord
   belongs_to :user
-  has_many :tasks, as: :taskable
+  has_many :tasks, as: :taskable, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 }
 end
