eb94812ec5467ba00e5db26cd12c0dca761590e8
gavinsharkey <gavinsharkey2016@gmail.com>
2020-10-27 12:57:02 -0400

    Add destroy route for task groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tasks, only: [:index, :create, :update, :destroy]
-  resources :task_groups, only: [:index, :create]
+  resources :task_groups, only: [:index, :create, :destroy]
   post '/login', to: 'auth#login'
   post '/signup', to: 'users#create'
   get '/logged_in', to: 'auth#is_logged_in?'
