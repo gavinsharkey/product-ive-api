6ec768d47b61720dc8e6dcb308f6f490387cb6bf
gavinsharkey <gavinsharkey2016@gmail.com>
2020-11-30 17:14:20 -0600

    Persist task

--- app/controllers/sub_tasks_controller.rb
+++ app/controllers/sub_tasks_controller.rb
@@ -8,10 +8,14 @@ class SubTasksController < ApplicationController
     task_id = params[:task_id]
     if Task.exists?(task_id)
       @task = Task.new(task_params.merge({ parent_task_id: task_id }))
+      if @task.save
+        render json: @task, include: [:taskable]
+      else
+        render json: { errors: @task.errors.full_messages }
+      end
     else
       render json: { error: 'Parent Resource Not Found' }, status: :not_found
     end
-    render json: @task, include: [:taskable]
   end
 
   private
